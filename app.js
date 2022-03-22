17247426641ad608c9024ea1d8e6374ee3560aa2
Vladlen <vladlengg@gmail.com>
2022-03-23 02:52:16 +0500

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const userId = "3755054421284641"
 
 // get accessToken from facebook developers website
 const client = new MessengerClient({
-    accessToken: 'EAALPPluYZBg8BABYzRI4XaUFJ4wbaJxMUOh09DzuEZBZBkJKV2fJwvZC6z65SyeteDDkluaINdlXuViakRV01guBEiZAEAJxwaK3BLTVsES2Yz8wbCr9JOCdHzoFGjplZACMXKIhBXKZBuCOmYg9yfDXWSNWHm0fTMgsNVspjI4pqaZChe1VPbMr',
+    accessToken: 'EAALPPluYZBg8BADZAV0AEFPqCjleHOuH3osooC8ZA5xKrZBwHTofzZA2xBqVnGV9s5d8RYUtLVwuD5tAW4kz4bXZBHhsD17BYGyjrjci1IEjbZC2H8fHfjZAYB1AtHZAfvtivJsYFf2knyZBkbFZCdurpgrjmx5CqQ06lZCudVhJst6ZB5GqMiyKqQL6q',
     appId: "790816685095439",
     appSecret: "013fac914053354b9d6991520e34312d",
     version: '13.0',
